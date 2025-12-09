610523eec1f8fe269ed83d4212eae8f96a07a5f5
Igor Monteiro <monteiroigor2022@gmail.com>
2025-12-09 20:45:40 -0300

    tudo certo

--- src/logica/IEstrategia.ts
+++ src/logica/IEstrategia.ts
@@ -4,10 +4,10 @@ export interface DadosClima {
     umidade: number;   // Umidade %
     vento: number;     // Velocidade do vento
     chuva: number;     // Chance de chuva %
+    data: string;
 }
 
 // Essa é a Interface 
 export interface IEstrategia {
-    // Todo mundo que usar essa interface é OBRIGADO a ter este método:
     avaliar(clima: DadosClima): string;
 }
