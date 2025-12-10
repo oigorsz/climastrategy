a40b267f3536515d3bd5f0c72d12fdd89c8cb38a
Bruna <brunasurur@gmail.com>
2025-12-09 21:56:49 -0300

    correcoes finais

--- src/strategies/IEstrategiaAtividade.ts
+++
@@ -1,9 +0,0 @@
-import { DadosClima, ResultadoVerificacao } from '../interfaces/clima.interfaces';
-
-/**
- * A interface Strategy declara o método que todas as estratégias
- * concretas devem implementar.
- */
-export interface IEstrategiaAtividade {
-  verificarApropriado(dados: DadosClima): ResultadoVerificacao;
-}
